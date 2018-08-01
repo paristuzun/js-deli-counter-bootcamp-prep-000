2942c7e4c22945acf955a27f2104780337405a17
Paris Tuzun <paristuzun@gmail.com>
2018-08-01 17:22:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(line) {
     if  (line.length === 0){
         return "The line is currently empty.";
     }
-    else { return "The line is currently: 1. " + line[0] +"", 2. "" + line[1];
+    else { return "The line is currently: 1. " + line[0] + "", 2. "" + line[1];
   }
   return "The line is currently:" + newArray;
   // returns the current line as a string
