037f4411f6f9822cc93d281ba77c7f73496cd434
Paris Tuzun <paristuzun@gmail.com>
2018-08-01 17:28:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function currentLine(line) {
     }
     else {
        for (var i = 0; i < number; i++){
-         var placeInLine = i;
+         var placeInLine = i+1;
          newArray.push(" " +placeInLine + ". "+line[i]);
        }
   }
